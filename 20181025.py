178edb486180c151cb88468a7298cb7af2674880
金九 <liumingze18@users.noreply.github.com>
2018-10-25 15:58:17 +0800

    2018-10-25 15:38

---
+++ 20181025.py
@@ -0,0 +1,20 @@
+#这是一行注释
+
+'''
+多行注释
+多行注释
+多行注释
+'''
+
+counter = 100
+miles = 100.0
+name = "test"
+
+print(counter)
+print(miles)
+print(name)
+
+if true:
+	print("true")
+else:
+	print("false")
